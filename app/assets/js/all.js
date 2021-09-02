70c23f86ac9eb217ce5c9758915d0bd3f080a69b
zhaokat <katiezhao25@gmail.com>
2021-09-02 17:54:14 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,26 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+
+  // $('.nav-link').click(function(){
+  //   $('.nav .nav-link').removeClass('active');
+  //   $(this).addClass('active');
+  // });
+
+  // swiper
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      // when window width is >= 992px (lg)
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
+
+});
